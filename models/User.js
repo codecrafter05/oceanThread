87dfd163cebea0975dcc4d227ffd763961a416dd
Hamad Alhibshi <hamad.alhibshi@gmail.com>
2023-08-01 19:11:07 +0300

    added models along with mongoose for database and is connected

---
+++ models/User.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  googleId: {
+    type: String,
+    required: true
+  },
+
+  email: {
+    type: String
+  },
+
+  avatar: {
+    type: String
+  },
+
+  threadsCreated: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Post',
+  }],
+
+  comments: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Comment',
+  }],
+
+  points: {
+    type: Number,
+    default: 0,
+  },
+
+  endorsements: {
+    type: Number,
+    default: 0
+  },
+
+  threadsSolved: {
+    type: Number,
+    default: 0
+  },
+
+  replies: {
+    type: Schema.Types.ObjectId,
+    ref: 'Reply'
+  }
+
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('User', userSchema);
