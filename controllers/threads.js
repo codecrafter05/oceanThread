b5c28cdfdef199ea51b0e731d8045832439ae742
Hamad Alhibshi <hamad.alhibshi@gmail.com>
2023-08-01 19:00:27 +0300

    routes are fully functional

---
+++ controllers/threads.js
@@ -0,0 +1,28 @@
+const Thread = require('../models/Thread');
+
+
+
+async function threadsPage(req, res,) {
+  try {
+    res.render('pages/threads');
+  }
+  catch (err) {
+    console.log(err)
+  }
+};
+
+async function threadsShow(req, res,) {
+  try {
+    let threadID = req.params.id;
+    res.render('pages/threads/:id');
+  }
+  catch (err) {
+    console.log(err)
+  }
+};
+
+
+
+module.exports = {
+  threadsPage,
+}
