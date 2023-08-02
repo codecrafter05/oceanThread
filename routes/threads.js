b25144c94bf8f2d6b1de2176c2e1bd4514ccdfed
Hamad Alhibshi <hamad.alhibshi@gmail.com>
2023-08-02 10:28:56 +0300

    form is working but required for threadType has been removed cuz it is not in the form yet

--- routes/threads.js
+++ routes/threads.js
@@ -5,4 +5,8 @@ const threadsCtrl = require('../controllers/threads');
 
 router.get('/threads', threadsCtrl.threadsPage)
 router.get('/threads/threads-new', threadsCtrl.threadsNew)
+router.get('/threads/:id', threadsCtrl.threadsShow)
+router.post('/threads', threadsCtrl.createThread)
+
+
 module.exports = router;
