5ffb4f9f42ddf7613394be3a78ec6ca04d64d101
Hamad Alhibshi <hamad.alhibshi@gmail.com>
2023-08-02 22:34:40 +0300

    did threads-update crud but its not working

--- controllers/profile.js
+++ controllers/profile.js
@@ -12,6 +12,30 @@ async function profilePage(req, res) {
   }
 }
 
+async function threadsUpdateShow(req, res,) {
+  try {
+    const thread = await Thread.findByIdAndUpdate(req.query.id)
+    res.render(`pages/threads-update`, { thread });
+  }
+  catch (err) {
+    console.log(err)
+  }
+};
+
+async function threadsUpdate(req, res,) {
+  try {
+    const thread = await Thread.findByIdAndUpdate(req.query.id)
+    res.redirect(`pages/profile`, { thread });
+  }
+  catch (err) {
+    console.log(err)
+  }
+};
+
+
+
 module.exports = {
   profilePage,
+  threadsUpdate,
+  threadsUpdateShow,
 }
