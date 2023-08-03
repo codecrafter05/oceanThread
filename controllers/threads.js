f911f14ef9aa2df1928be7a9262735bb99c31cde
Ahmed Almadhoob <ahmed.almadhoob@souqalmanama.com>
2023-08-03 15:47:27 +0300

    Added create comment and reply to threads control

--- controllers/threads.js
+++ controllers/threads.js
@@ -1,46 +1,63 @@
 const Thread = require('../models/Thread');
 const User = require('../models/User');
+const Comment = require('../models/Comment');
+const Reply = require('../models/Reply');
 
-
-async function threadsPage(req, res,) {
+async function threadsPage(req, res) {
   try {
     const threads = await Thread.find();
     const users = await User.find();
     res.render('pages/threads', { threads, users });
+  } catch (err) {
+    console.log(err);
   }
-  catch (err) {
-    console.log(err)
-  }
-};
+}
 
-async function threadsShow(req, res,) {
+async function threadsShow(req, res) {
   try {
-    const thread = await Thread.findById(req.query.id)
+    const thread = await Thread.findById(req.query.id);
     res.render('pages/thread-view', { thread });
+  } catch (err) {
+    console.log(err);
   }
-  catch (err) {
-    console.log(err)
-  }
-};
+}
 
-async function threadsNew(req, res,) {
+async function threadsNew(req, res) {
   try {
     const users = await User.find();
     res.render('pages/threads-new', { users });
+  } catch (err) {
+    console.log(err);
   }
-  catch (err) {
-    console.log(err)
-  }
-};
+}
 
 async function createThread(req, res) {
   try {
     const thread = await Thread.create(req.body);
-    res.redirect('/threads')
-  }
-  catch (err) {
-    console.log(err)
+    res.redirect('/threads');
+  } catch (err) {
+    console.log(err);
     res.render('pages/threads-new');
+  }
+}
+
+async function createComment(req, res) {
+  try {
+    const comment = await Comment.create(req.body);
+    res.redirect('/thread-view');
+  } catch (err) {
+    console.log(err);
+    res.render('pages/thread-view');
+  }
+}
+
+async function createReply(req, res) {
+  try {
+    const reply = await Reply.create(req.body);
+    res.redirect('/thread-view');
+  } catch (err) {
+    console.log(err);
+    res.render('pages/thread-view');
   }
 }
 
@@ -49,4 +66,6 @@ module.exports = {
   threadsShow,
   threadsNew,
   createThread,
-}
+  createComment,
+  createReply,
+};
