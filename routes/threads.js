2f2607b21a8715d15e507e8d23722e5f10ef678a
Ahmed Almadhoob <ahmed.almadhoob@souqalmanama.com>
2023-08-05 01:39:33 +0300

    Showing comments partially on the frontpage

--- routes/threads.js
+++ routes/threads.js
@@ -5,7 +5,7 @@ const threadsCtrl = require('../controllers/threads');
 router.get('/threads', threadsCtrl.threadsPage);
 router.get('/threads/thread-view', threadsCtrl.threadsShow);
 router.post('/threads/thread-view', threadsCtrl.createComment);
-router.post('/threads/thread-view', threadsCtrl.createReply);
+// router.post('/threads/thread-view', threadsCtrl.createReply);
 router.get('/threads/threads-new', threadsCtrl.threadsNew);
 router.post('/threads/:id', threadsCtrl.createThread);
 
